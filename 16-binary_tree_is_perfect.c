c0bd8512029e293083ae92da786de15051f79b89
Hatim Selmun <selmun13@gmail.com>
2023-10-31 22:55:41 +0100

    ficing height

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -97,7 +97,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	if (tree->left == NULL &&  tree->right == NULL)
 		return (0);
